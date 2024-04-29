4e216432c858a55a1a77969752faef54b13ac6b8
windanws <winstondang00@gmail.com>
2024-04-29 21:06:13 +1000

    initial Commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import socket
+import dpkt
+import pyshark
+import networkx as nx
+import matplotlib.pyplot as plt
+
+pcapOP = 'wlan.pcap'
+
+def getPackets(pcap):
+    packets = [] 
+    timestamp = []
+
+    with open(pcap, 'rb') as f:
+        pcap = dpkt.pcap.Reader(f) 
+        for (ts, buf) in pcap:
+            try:
+                eth = dpkt.ethernet.Ethernet(buf)
+
+                if not hasattr(eth.data, 'src'):
+                    continue
+
+                ip = eth.data
+                src = socket.inet_ntoa(ip.src)
+                dst = socket.inet_ntoa(ip.dst)
+                packets.append((src, dst))
+                timestamp.append(ts)
+            except KeyboardInterrupt:
+                sys.exit(0)
+            except:
+                continue
+    return packets, timestamp
+
+
+
+def main():
+    packets, timestamps  = getPackets(pcapOP)
+
+    for src_ip, dst_ip in packets:
+        print(f"Source IP: {src_ip}\nDestination IP: {dst_ip}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
