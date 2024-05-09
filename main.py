65520ab5a2995aa3bce8f6f5b2cef55295fa2598
windanws <winstondang00@gmail.com>
2024-05-10 00:02:46 +1000

    Increased Graph Loading for more nodes

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+
 import time
+import re
+
 import socket
 import dpkt
-import networkx as nx
+import mmap
 import pandas as pd
-import argparse
+import networkx as nx
 from pyvis.network import Network
+
+import argparse
 
 
 def getPackets(pcap):
@@ -12,6 +18,7 @@ def getPackets(pcap):
     timestamp = []
 
     with open(pcap, 'rb') as f:
+        mmapFile = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
         pcap = dpkt.pcap.Reader(f) 
         for (ts, buf) in pcap:
             try:
@@ -27,7 +34,9 @@ def getPackets(pcap):
                 sys.exit(0)
             except: 
                 continue
-
+        
+        mmapFile.close()
+        
     return packets, timestamp
 
 
@@ -55,14 +64,46 @@ def graphGen(df, sampleAmount, options):
     if options:
         nt.width="70%"
         nt.show_buttons()
+
+    if sampleAmount > 100:
+        pos = nx.circular_layout(G, scale=sampleAmount*2)
+        for node in nt.get_nodes():
+            nt.get_node(node)['x']=pos[node][0]
+            nt.get_node(node)['y']=-pos[node][1]
+            nt.get_node(node)['physics']=False
+            nt.get_node(node)['label']=str(node)
+        nt.toggle_physics(False)
+
     nt.show("graph.html", notebook=False)
 
 
 
-def printPackets(packets):
+def countPackets(df, ipAddr):
+    out = df.eq(ipAddr).any()
+    print(out)
+
+
+
+def listAddress(df, ipAddr):
+    print("=== Matching Beginning ===")
+    matching = df[df.apply(lambda row: ipAddr in row.values, axis=1)]
+    
+    if not matching.empty:
+        print("\nFound Matching - Beginning Printing Processs")
+        for index, row in matching[['Source IP','Destination IP']].iterrows():
+            src_ip = row['Source IP']
+        dst_ip = row['Destination IP']
+        print(f"Source IP: {src_ip} - Destination IP: {dst_ip}\n")
+    else:
+        print("\nNo Match Found\nPlease Ensure the Correct Address is Inputted")
+
+
+
+def test():
     for src_ip, dst_ip in packets:
         print(f"Source IP: {src_ip}\nDestination IP: {dst_ip}\n")
-    
+
+
 
 
 def getArgs(argv=None):
@@ -74,7 +115,9 @@ def getArgs(argv=None):
     parser.add_argument("-g", "--graph", action="store_true", help = "Create Network Graph from PCAP File")
     parser.add_argument("-o", "--options", action="store_true", help = "Show Options in Graph Page") 
 
+    parser.add_argument("-l", "--list", action="store", help = "")
     parser.add_argument("-n", "--number", nargs="?", const=100, default=100, type=int, help = "Number of Nodes in Graph")
+    parser.add_argument("-c", "--count", action="store", help = "Count specific IP address\n Left Empty Count Total in Provided File")
 
     return parser.parse_args(argv)
 
@@ -83,11 +126,16 @@ def getArgs(argv=None):
 def main():
     args = getArgs()
 
+
     if args.filename.endswith(".pcap"):
         packets, timestamps = getPackets(args.filename)
         df = dataFrameNetwork(packets, timestamps, args.save)
         if args.graph:
             graphGen(df, args.number, args.options)
+        elif args.count:
+            countPackets(df, arg.count)
+        elif args.list:
+            listAddress(df, args.list)
         else:
             pass
     elif args.filename.endswith(".csv"):
@@ -95,7 +143,7 @@ def main():
     else:
         print("Invaild File Type \nPlease Use -h to Get More Information")
         pass
-    
+
 
 
 
